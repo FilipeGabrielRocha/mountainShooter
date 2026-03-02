a41f02b2a3c23264792f2fc3bebc573e61b89691
Filipe Gabriel Rocha <94459039+FilipeGabrielRocha@users.noreply.github.com>
2026-03-02 11:31:33 -0300

    Adding texts to the menu

--- code/Menu.py
+++ code/Menu.py
@@ -1,4 +1,9 @@
 import pygame.image
+from pygame.font import Font
+from pygame.rect import Rect
+from pygame.surface import Surface
+
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
 
 
 class Menu:
@@ -18,10 +23,25 @@ class Menu:
             # 3- desenha a imagem no retângulo
             self.window.blit(source=self.surf, dest=self.rect)
             # 4- Atualiza para a imagem aparecer
+
+            # Desenhando o texto
+            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range (len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
             pygame.display.flip()
 
             # Check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit() #Close window
-                    quit() #End pygame
+                    pygame.quit()  # Close window
+                    quit()  # End pygame
+
+    # Criando os texto no menu
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
