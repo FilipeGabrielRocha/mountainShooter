886987c8b8e2044a88a1374ba4cd006d99a7d832
Filipe Gabriel Rocha <94459039+FilipeGabrielRocha@users.noreply.github.com>
2026-03-02 10:55:05 -0300

    Adding assets to game folder Adding imagem from menu

--- code/Menu.py
+++ code/Menu.py
@@ -1,6 +1,17 @@
+import pygame.image
+
+
 class Menu:
     def __init__(self, window):
         self.window = window
+        # 1- carrego a imagem
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+        # 2- posiciono a imagem
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
+        # 3- desenha a imagem no retângulo
+        self.window.blit(source=self.surf, dest=self.rect)
+        # 4- Atualiza para a imagem aparecer
+        pygame.display.flip()
         pass
