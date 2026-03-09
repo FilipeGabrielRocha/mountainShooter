1168763fac48ad53fe88eb2de9f4594d44c7288c
Filipe Gabriel Rocha <94459039+FilipeGabrielRocha@users.noreply.github.com>
2026-03-09 13:20:17 -0300

    Creating player shot creating enemy shot

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,14 +1,24 @@
 import pygame.key
 
-from code.Const import ENTITY_SPEED, WIN_WIDTH
+from code.Const import ENTITY_SPEED, WIN_WIDTH, PLAYER_KEY_SHOOT, ENTITY_SHOT_DELAY
+from code.EnemyShot import EnemyShot
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
+
 
 class Enemy(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
     def update(self):
         pass
 
     def move(self):
         self.rect.centerx -= ENTITY_SPEED[self.name]
+
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
